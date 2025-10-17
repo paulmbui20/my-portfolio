e69e5c7735a9a7f0f873cb36fb7746a6fa49ae67
Paul Mbui <paulmbui20@gmail.com>
2025-10-18 01:47:40 +0300

    basic setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Navbar from '$lib/components/ui/navbar.svelte';
+	import Footer from '$lib/components/ui/footer.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Paul Mbui - My Portfolio</title>
+	<meta name="description" content="Paul Mbui - My Portfolio" />
 </svelte:head>
 
-{@render children?.()}
+<body>
+	<section>
+		<Navbar />
+	</section>
+	<main>
+		{@render children?.()}
+	</main>
+	<footer>
+		<Footer />
+	</footer>
+</body>
